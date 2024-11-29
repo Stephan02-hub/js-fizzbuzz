b7b2868e3537a29194a007f4cb605362de8ec789
Stephan02-hub <braghesesteven@gmail.com>
2024-11-29 15:30:36 +0100

    feat:add number

---
+++ script.js
@@ -0,0 +1,16 @@
+//crea un ciclo che va da 1 a 100
+for(let i = 1; 1<=100; i++){
+    //se il numero è divisibile per 3
+    if(i % 3 === 0){
+        console.log("frizz")
+    }//altrimenti s'è divisbile per 5
+    else if (i % 5 === 0){
+        console.log("buzz")
+    }//altrimenti s'è divisibile sia per 3 che per 5
+    else if(i % 3 === 0 && i % 5 === 0){
+        console.log("frizzbuzz")
+    }// se  non è disvisibile sia per 3 che per 5
+    else{
+        console.log(i)
+    }
+}
